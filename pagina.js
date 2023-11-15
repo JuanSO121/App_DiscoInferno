d16c06bdf7b62a45878f10d5e0f62cdc4ac14a39
juanSO121 <126177797+JuanSO121@users.noreply.github.com>
2023-11-15 00:50:27 -0500

    Alpha

--- pagina.js
+++ pagina.js
@@ -14,3 +14,18 @@ function showContent(contentId) {
         content.style.display = 'none';
     }
 }
+
+function showElement(elementName, elementDescription, elementImage) {
+    // Construir el contenido del modal con información detallada del elemento seleccionado
+    var modalContent = `
+        <img src="${elementImage}" class="img-fluid" alt="${elementName}">
+        <h5>${elementName}</h5>
+        <p>${elementDescription}</p>
+    `;
+
+    // Cambiar el contenido del modal
+    document.getElementById('modal-body').innerHTML = modalContent;
+
+    // Mostrar el modal
+    $('#myModal').modal('show');
+}
