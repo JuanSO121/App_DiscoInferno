4421bab27eaa15e30baac7b1456e968879f2e1bd
juanSO121 <126177797+JuanSO121@users.noreply.github.com>
2023-11-15 16:39:25 -0500

    Alpha

--- pagina.js
+++ pagina.js
@@ -1,19 +1,26 @@
-function showMessage(message) {
-    // Cambiar el contenido del modal
-    document.getElementById('modal-body').innerHTML = message;
+function showContent(contentType) {
+    // Desactiva todos los contenidos
+    document.getElementById("reglas").style.display = "none";
+    document.getElementById("elementos").style.display = "none";
 
-    // Mostrar el modal
-    $('#myModal').modal('show');
-}
-
-function showContent(contentId) {
-    var content = document.getElementById(contentId);
-    if (content.style.display === 'none') {
-        content.style.display = 'block';
-    } else {
-        content.style.display = 'none';
+    // Muestra el contenido seleccionado
+    if (contentType === 'reglas') {
+        document.getElementById("reglas").style.display = "block";
+    } else if (contentType === 'elementos') {
+        document.getElementById("elementos").style.display = "block";
     }
 }
+
+function showMessage(contentType) {
+    // Aquí puedes realizar acciones específicas al hacer clic en un mensaje si es necesario
+    // Por ahora, solo activamos el contenido correspondiente
+    showContent(contentType);
+}
+
+// Añade este evento al cargar la página para asegurarte de que uno de los contenidos esté visible
+window.onload = function () {
+    showContent('reglas'); // Puedes cambiar esto a 'elementos' si deseas que el otro contenido esté activo inicialmente
+};
 
 function showElement(elementName, elementDescription, elementImage) {
     // Construir el contenido del modal con información detallada del elemento seleccionado
